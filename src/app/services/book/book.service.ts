6c63e9f3571dca925ec88e7db57229786cb4b923
Ubuntu <ubuntu@ip-172-31-38-189.us-west-2.compute.internal>
2018-01-12 01:54:41 +0000

    ubuntu needs properly capitalized filenames perhaps

--- src/app/services/book/book.service.ts
+++ src/app/services/book/book.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import 'rxjs/add/operator/map';
 
-import { Book } from '../../models/Book' ;
+import { Book } from '../../models/book' ;
 
 @Injectable()
 export class BookService {
